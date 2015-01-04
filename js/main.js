de6d8a99d5386e301a3e74b7f80fa9379122f4e4
vikixiu <vicky.wu1220@gmail.com>
2015-01-04 15:53:43 +0800

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ $(document).ready(function (argument) {
 	$('#header').load('header.html', function(){
 		$('#nav a:eq('+currentNav+')').addClass('current');
 	});
-	$('#footer').load('footer.html');
+	$('#footer').load('footer.html', function(){
+		$('.showlink2').hover(function(){
+			$('.footerlinks1, .footerlinks2').toggleClass('hide');
+			$('.showlink2>a').toggleClass('blue')
+		})
+	});
+
 
 	
 })
